4c142b711c50310c8f4aa23e0357ac7ec947c2c5
Nicole Cedano <cedano.nicolee@gmail.com>
2019-05-13 15:52:38 -0600

    06 deployed

--- type-ahead/app.js
+++ type-ahead/app.js
@@ -1,4 +1,4 @@
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
 
 const cities = []
 
@@ -22,8 +22,8 @@ function displayMatches(){
     const matchArray = findMatches(this.value, cities)
     const html = matchArray.map(place => {
         const regex = new RegExp(this.value, 'gi')
-        const cityName = place.city.replace(regex, `<span class="h1">${this.value}</span>`)
-        const stateName = place.state.replace(regex, `<span class="h1">${this.value}</span>`)
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`)
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`)
 
         return `
             <li>
