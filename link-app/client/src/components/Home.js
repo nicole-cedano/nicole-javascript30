b0a3fcf08bc5bcbb46ece989ac40470155aeacf9
Nicole Cedano <cedano.nicolee@gmail.com>
2019-05-18 13:11:03 -0500

    ex 8 complete

--- link-app/client/src/components/Home.js
+++ link-app/client/src/components/Home.js
@@ -22,6 +22,7 @@ const Home = () => {
                 <a href="http://ncflexpanelgallery.surge.sh/" target="_blank" rel="noopener noreferrer">Day 05: Flex Panel Gallery</a>
                 <a href="http://nctypeahead.surge.sh/" target="_blank" rel="noopener noreferrer">Day 06: Type Ahead</a>
                 <a href="http://ncarraycardio2.surge.sh/" target="_blank" rel="noopener noreferrer">Day 07: Array Cardio: 2</a>
+                <a href="http://nchtmlcanvas.surge.sh/" target="_blank" rel="noopener noreferrer">Day 08: HTML Canvas Fun</a>
           
             </div>
         </div>
